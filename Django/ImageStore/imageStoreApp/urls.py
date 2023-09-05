87a4f1e36216c0fded8581a2a551fe9598784cf3
Victor Polyakov <polyakov.victor21@gmail.com>
2023-09-05 03:10:04 +0300

    fast-api forms with auth

--- Django/ImageStore/imageStoreApp/urls.py
+++ Django/ImageStore/imageStoreApp/urls.py
@@ -6,7 +6,4 @@ app_name = 'imageStoreApp'
 urlpatterns = [
     re_path('', views.home_view, name='home'),
     re_path('pin/<int:id>', views.pin_detail, name='pin_detail'),
-    re_path('login/', views.login_view, name='login'),
-    re_path('registration/', views.register, name='registration'),
-    re_path('logout/', views.logout_view, name='logout')
 ]
