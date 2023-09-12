aaef9c2e95ee3565fbb1337bed3b94f75bac69a0
Victor <polyakov.victor21@gmail.com>
2023-09-12 14:23:59 +0300

    add poetry packege

--- FastAPI/config.py
+++ FastAPI/config.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
+print("DATABASE URL", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
