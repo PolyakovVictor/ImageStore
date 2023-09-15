5ad118033d561d3cc8f8169ffea1dfe14293cca9
Victor <polyakov.victor21@gmail.com>
2023-09-15 19:33:42 +0300

    remove jwt auth

--- Django/ImageStore/account/urls.py
+++ Django/ImageStore/account/urls.py
@@ -6,8 +6,5 @@ app_name = 'account'
 urlpatterns = [
     re_path('login/', views.login_view, name='login'),
     re_path('register/', views.register_view, name='register'),
-    re_path('login_api/', views.login_api, name='login_api'),
-    re_path('register_api/', views.register_api.as_view(), name='register_api'),
-    re_path('test_token/', views.test_token),
     re_path('logout/', views.logout_view, name='logout'),
 ]
--- Django/ImageStore/account/views.py
+++ Django/ImageStore/account/views.py
@@ -1,80 +1,36 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import UserSerializer
-from rest_framework import status
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-from django.contrib.auth import logout
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import get_object_or_404, redirect, render
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-
-
-class register_api(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password1 = request.data.get('password1')
-        password2 = request.data.get('password2')
-        email = request.data.get('email')
-        print("VIEWS: ", username, password1, password2, email)
-
-        if not username or not email or not password2 or not password1:
-            return Response({'error': 'All fields are required.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
-            return Response({'error': 'User with this username or email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if password1 == password2:
-
-            user = User.objects.create_user(username=username, password=password1, email=email)
-            user.save()
-
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            return Response({'token': access_token}, status=status.HTTP_201_CREATED)
+from .forms import RegistrationForm, LoginForm
 
 
 def login_view(request):
-    return render(request, 'account/login.html')
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('imageStoreApp:home')
+        else:
+            messages.error(request, "Incorrect username or password")
+    return render(request, 'account/login.html', {"form": form})
 
 
 def register_view(request):
-    print("TEST")
-    return render(request, 'account/registration.html')
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("account:login")
+        else:
+            messages.error(request, "Error, incorrect data. Please, try again.")
+    else:
+        form = RegistrationForm()
+    return render(request, "account/registration.html", {"form": form})
 
 
 def logout_view(request):
     logout(request)
     return redirect('imageStoreApp:home')
-
-
-@api_view(['POST'])
-def login_api(request):
-    user = get_object_or_404(User, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
-
-
-# @api_view(['POST'])
-# def signup(request):
-#     serializer = UserSerializer(data=request.data)
-#     if serializer.is_valid():
-#         user = serializer.save()
-#         user.set_password(request.data['password'])
-#         user.save()
-#         token, created = Token.objects.get_or_create(user=user)
-#         return Response({"token": token.key, "user": serializer.data})
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def test_token(request):
-    return Response("passed for {}".format(request.user.email))
